6b3c50223669340f540d8ce13e19f5f3e352e0a5
AlSophia <92183447+AlSophia@users.noreply.github.com>
2021-11-14 23:51:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-k=int(input())
-m=list()
-lists=list()
-for i in range(0,k):
-    l=input(list())
-    m+=l
+lists=[]
 def merge(*lists):
+    m=sum(lists,[])
     lists=sorted(m)
     print(lists)
-#Interesting fact:In six hours, the world’s deserts receive more energy from the sun than humans consume in a year
-merge(*lists)
+#In six hours, the world’s deserts receive more energy from the sun than humans consume in a year
+
+merge([3,2,4,5,2,5,2],[5,4,5,6,6],[2])
+merge([3,2,5,2,2],[5,4,5,6],[2,2,1,8,3])
