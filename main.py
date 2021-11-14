6057cbd62cd0cb5f414fe31a045177debed1f384
AlSophia <92183447+AlSophia@users.noreply.github.com>
2021-11-14 17:18:41 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+k=int(input())
+m=list()
+lists=list()
+for i in range(0,k):
+    l=input(list())
+    m+=l
+def merge(*lists):
+    lists=sorted(m)
+    print(lists)
+#Interesting fact:In six hours, the world’s deserts receive more energy from the sun than humans consume in a year
+merge(*lists)
